323dffa6bae0aa2f63584aec0dcb4b80e2b4bb49
Guillaume Revaillot <revaillot@archos.com>
2019-01-10 18:41:56 +0100

    stm32g0: add flash.
    here, it's a bit of a mess.. G0 flash controller does not really
    match exsting feature split. IE it has instruction cache only ..
    so, no flash_idcache.c as it. flash_common_f could be used, but
    flash_unlock would not take care of option byte ?
    
    prefetch, icache and lock is ok. I had no look at flash programming
    or erase yet..

--- include/libopencm3/stm32/flash.h
+++ include/libopencm3/stm32/flash.h
@@ -38,6 +38,8 @@
 #       include <libopencm3/stm32/l1/flash.h>
 #elif defined(STM32L4)
 #       include <libopencm3/stm32/l4/flash.h>
+#elif defined(STM32G0)
+#       include <libopencm3/stm32/g0/flash.h>
 #else
 #       error "stm32 family not defined."
 #endif
